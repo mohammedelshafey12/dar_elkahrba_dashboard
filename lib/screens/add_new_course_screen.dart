674399f932bc00e8173530aaef4527759bfafa2b
Aasem <assem_soma541@yahoo.com>
2021-08-07 20:01:24 +0200

    Add new/edit course functionality and courses page

--- lib/screens/add_new_course_screen.dart
+++ lib/screens/add_new_course_screen.dart
@@ -1,15 +1,88 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:typed_data';
 
-class AddNewCourseScreen extends StatelessWidget {
+import 'package:dar_elkahrba/providers/course.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
+
+class AddNewCourseScreen extends StatefulWidget {
   static const String routeName = '/Add-new-course';
-  final List<String> hints = [
-    'Course title',
-    'Course instructor',
-    'Course description',
-    'Course hours',
-    'Course price',
-    'Course PDF Url',
-  ];
+
+  @override
+  _AddNewCourseScreenState createState() => _AddNewCourseScreenState();
+}
+
+class _AddNewCourseScreenState extends State<AddNewCourseScreen> {
+  var _isInit = true;
+  var _isUploading = false;
+  var _pdfName = '';
+
+  var _editedCourse = Course(
+      id: '',
+      title: '',
+      instructor: '',
+      hours: 0,
+      price: 0.0,
+      description: '',
+      pdfUrl: '');
+
+  var initialValues = {
+    'title': '',
+    'instructor': '',
+    'hours': '',
+    'price': '',
+    'description': '',
+    'pdfUrl': ''
+  };
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    if (_isInit) {
+      final id = ModalRoute.of(context)!.settings.arguments as String;
+      if (id.isNotEmpty) {
+        final loadedCourse = Provider.of<CoursesProvider>(context)
+            .courses
+            .firstWhere((course) => course.id == id);
+        _editedCourse = loadedCourse;
+        initialValues = {
+          'title': _editedCourse.title,
+          'instructor': _editedCourse.instructor,
+          'description': _editedCourse.description,
+          'hours': _editedCourse.hours.toString(),
+          'price': _editedCourse.price.toString(),
+          'pdfUrl': _editedCourse.pdfUrl,
+        };
+      }
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
+
+  void _saveForm() {
+    final isValidate = _formKey.currentState!.validate();
+    if (!isValidate) {
+      return;
+    }
+    _formKey.currentState!.save();
+    if (_editedCourse.id.isNotEmpty) {
+      Provider.of<CoursesProvider>(context, listen: false)
+          .updateCourse(_editedCourse, _editedCourse.id);
+    } else {
+      Provider.of<CoursesProvider>(context, listen: false)
+          .addCourse(_editedCourse);
+      print(_editedCourse.instructor);
+
+      print(_editedCourse.pdfUrl);
+    }
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
@@ -19,72 +92,224 @@ class AddNewCourseScreen extends StatelessWidget {
         title: const Text('Add new course'),
       ),
       body: Center(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Container(
-              padding: EdgeInsets.all(10),
-              width: deviceSize.width * 0.7,
-              height: deviceSize.height * 0.8,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15)),
-                child: Form(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          ...hints
-                              .map((hint) => AddNewCourseTxtFld(hint))
-                              .toList(),
-                        ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                width: deviceSize.width * 0.7,
+                height: deviceSize.height * 0.8,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Form(
+                    key: _formKey,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.all(10),
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(width: 1)),
+                              child: TextFormField(
+                                initialValue: initialValues['title'],
+                                onSaved: (value) {
+                                  _editedCourse =
+                                      _editedCourse.copyWith(title: value);
+                                },
+                                validator: (value) {
+                                  if (value!.isEmpty)
+                                    return 'Please Enter the title';
+                                  else
+                                    return null;
+                                },
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    contentPadding: EdgeInsets.all(5),
+                                    hintText: 'Course Title'),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.all(10),
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(width: 1)),
+                              child: TextFormField(
+                                initialValue: initialValues['instructor'],
+                                onSaved: (value) {
+                                  _editedCourse =
+                                      _editedCourse.copyWith(instructor: value);
+                                },
+                                validator: (value) {
+                                  if (value!.isEmpty)
+                                    return 'Please Enter the instructor name';
+                                  else
+                                    return null;
+                                },
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    contentPadding: EdgeInsets.all(5),
+                                    hintText: 'Course Instructor'),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.all(10),
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(width: 1)),
+                              child: TextFormField(
+                                initialValue: initialValues['description'],
+                                onSaved: (value) {
+                                  _editedCourse = _editedCourse.copyWith(
+                                      description: value);
+                                },
+                                validator: (value) {
+                                  if (value!.isEmpty)
+                                    return 'Please Enter the description';
+                                  else
+                                    return null;
+                                },
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    contentPadding: EdgeInsets.all(5),
+                                    hintText: 'Course Description'),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.all(10),
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(width: 1)),
+                              child: TextFormField(
+                                initialValue: initialValues['hours'],
+                                onSaved: (value) {
+                                  _editedCourse = _editedCourse.copyWith(
+                                      hours: int.parse(value!));
+                                },
+                                validator: (value) {
+                                  if (value!.isEmpty)
+                                    return 'Please Enter the hours';
+                                  if (double.tryParse(value) == null)
+                                    return 'Please Enter a number';
+                                  if (double.tryParse(value)! < 0)
+                                    return 'Please Enter a valid number';
+                                  else
+                                    return null;
+                                },
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    contentPadding: EdgeInsets.all(5),
+                                    hintText: 'Course Hours'),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.all(10),
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(width: 1)),
+                              child: TextFormField(
+                                initialValue: initialValues['price'],
+                                onSaved: (value) {
+                                  _editedCourse = _editedCourse.copyWith(
+                                      price: double.parse(value!));
+                                },
+                                validator: (value) {
+                                  if (value!.isEmpty)
+                                    return 'Please Enter the price';
+                                  if (double.tryParse(value) == null)
+                                    return 'Please Enter a numeric price';
+                                  if (double.tryParse(value)! < 0)
+                                    return 'Please Enter a valid price';
+                                  else
+                                    return null;
+                                },
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    contentPadding: EdgeInsets.all(5),
+                                    hintText: 'Course Price'),
+                              ),
+                            ),
+                            (_isUploading)
+                                ? Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: CircularProgressIndicator(),
+                                  )
+                                : Container(
+                                    width: deviceSize.width * 0.2,
+                                    child: Card(
+                                      elevation: 3,
+                                      child: InkWell(
+                                        onTap: _openPicker,
+                                        child: ListTile(
+                                          leading: CircleAvatar(
+                                            child: Text('PDF'),
+                                          ),
+                                          title: Text((_pdfName.isEmpty)
+                                              ? 'Click to add course brochure'
+                                              : _pdfName),
+                                          trailing: Icon(Icons.add),
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                'Add',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(Color.fromRGBO(38, 231, 122, 1)),
-                minimumSize: MaterialStateProperty.all(
-                  Size(deviceSize.height * 0.2, deviceSize.width * 0.04),
+              ElevatedButton(
+                onPressed: (_isUploading) ? null : _saveForm,
+                child: Text(
+                  'Done',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                 ),
-              ),
-            )
-          ],
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      Color.fromRGBO(38, 231, 122, 1)),
+                  minimumSize: MaterialStateProperty.all(
+                    Size(deviceSize.height * 0.2, deviceSize.width * 0.04),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
-}
 
-class AddNewCourseTxtFld extends StatelessWidget {
-  final String hint;
+  void _openPicker() async {
+    final result = await FilePicker.platform
+        .pickFiles(type: FileType.custom, allowedExtensions: ['pdf']);
+    if (result != null) {
+      final uploadedPdf = result.files.single.bytes;
+      final loadedPdfName = result.files.single.name;
+      setState(() {
+        _isUploading = true;
+      });
+      Reference reference =
+          FirebaseStorage.instance.ref().child('${Uuid().v1()}.pdf');
+      final uploadTask = reference.putData(uploadedPdf!);
 
-  AddNewCourseTxtFld(this.hint);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(width: 1)),
-      child: TextFormField(
-        decoration: InputDecoration(
-            border: InputBorder.none,
-            contentPadding: EdgeInsets.all(5),
-            hintText: hint),
-      ),
-    );
+      uploadTask.whenComplete(() async {
+        final pdf = await uploadTask.snapshot.ref.getDownloadURL();
+        _editedCourse = _editedCourse.copyWith(pdfUrl: pdf);
+        setState(() {
+          _pdfName = loadedPdfName;
+          _isUploading = false;
+        });
+      });
+    }
   }
 }
--- lib/screens/authentication/login_screen.dart
+++ lib/screens/authentication/login_screen.dart
@@ -1,13 +1,13 @@
-import 'package:dar_elkahrba/widget/default_input.dart';
-import 'package:dar_elkahrba/widget/label_of_text_form_field.dart';
+import 'package:dar_elkahrba/widgets/default_input.dart';
+import 'package:dar_elkahrba/widgets/label_of_text_form_field.dart';
 import 'package:flutter/material.dart';
 
-class LogInScreen extends StatefulWidget {
+class LoginScreen extends StatefulWidget {
   @override
-  _LogInScreenState createState() => _LogInScreenState();
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LogInScreenState extends State<LogInScreen> {
+class _LoginScreenState extends State<LoginScreen> {
   var usernameController = TextEditingController();
 
   var passwordController = TextEditingController();
@@ -26,60 +26,83 @@ class _LogInScreenState extends State<LogInScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Image.asset('log_in_screen/icon_left.png',),
-              SizedBox(height: 20.0,),
+              Image.asset(
+                'authentication/icon_left.png',
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
               Text(
                 'Sign In',
                 style: TextStyle(
                   fontSize: 42.0,
                 ),
               ),
-              SizedBox(height: 40.0,),
-              LabelOfTextFormFiled(label: 'USERNAME',),
-              SizedBox(height: 20.0,),
+              SizedBox(
+                height: 40.0,
+              ),
+              LabelOfTextFormFiled(
+                label: 'USERNAME',
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
               DefaultInput(
                 controller: usernameController,
                 type: TextInputType.text,
-                validator: (value){
-                  if (value!.isEmpty){
+                validator: (value) {
+                  if (value!.isEmpty) {
                     return 'username must be not null';
                   }
                   return null;
                 },
               ),
-              SizedBox(height: 20.0,),
-              LabelOfTextFormFiled(label: 'PASSWORD',),
-              SizedBox(height: 20.0,),
+              SizedBox(
+                height: 20.0,
+              ),
+              LabelOfTextFormFiled(
+                label: 'PASSWORD',
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
               DefaultInput(
                 controller: passwordController,
                 obscureText: passwordIsHidden,
                 type: TextInputType.visiblePassword,
-                validator: (value){
-                  if (value!.isEmpty){
+                validator: (value) {
+                  if (value!.isEmpty) {
                     return 'password must be not null';
                   }
                   return null;
                 },
-                suffixIcon: passwordIsHidden?Icons.visibility : Icons.visibility_off,
-                onTapSuffixIcon: (){
+                suffixIcon:
+                    passwordIsHidden ? Icons.visibility : Icons.visibility_off,
+                onTapSuffixIcon: () {
                   setState(() {
                     passwordIsHidden = !passwordIsHidden;
                   });
                 },
               ),
-              SizedBox(height: 20.0,),
+              SizedBox(
+                height: 20.0,
+              ),
               Container(
                 width: double.infinity,
                 height: 50.0,
                 child: MaterialButton(
-                  onPressed: (){
-                    if (formKey.currentState!.validate()){
-
-                    }
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {}
                   },
                   height: 50.0,
-                  child: Text('Sign In', style: TextStyle(color: Colors.white, fontSize: 20,
-                    letterSpacing: 0.5,),),
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      letterSpacing: 0.5,
+                    ),
+                  ),
                   focusColor: Colors.green,
                   elevation: 0.0,
                   focusElevation: 0.0,
@@ -87,7 +110,7 @@ class _LogInScreenState extends State<LogInScreen> {
                   highlightElevation: 0.0,
                   color: Colors.green,
                   shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
+                    borderRadius: BorderRadius.circular(15.0),
                   ),
                 ),
               ),
