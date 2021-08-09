af206630de10e5db003acc9971bf8b70d8df86c4
Ahmed Saad <a7md.s3d.2001@gmail.com>
2021-08-09 14:01:41 +0200

    qr ui

--- lib/screens/authentication/login_screen.dart
+++ lib/screens/authentication/login_screen.dart
@@ -1,3 +1,5 @@
+import 'package:dar_elkahrba/screens/authentication/sign_up_screen.dart';
+import 'package:dar_elkahrba/widgets/default_button.dart';
 import 'package:dar_elkahrba/widgets/default_input.dart';
 import 'package:dar_elkahrba/widgets/label_of_text_form_field.dart';
 import 'package:flutter/material.dart';
@@ -8,111 +10,181 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  var usernameController = TextEditingController();
-
+  var emailController = TextEditingController();
   var passwordController = TextEditingController();
-
   var formKey = GlobalKey<FormState>();
-
   bool passwordIsHidden = true;
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Form(
-          key: formKey,
+      body: Center(
+        child: Container(
+          width: width * 0.7,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset(
-                'authentication/icon_left.png',
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                'Sign In',
-                style: TextStyle(
-                  fontSize: 42.0,
-                ),
-              ),
-              SizedBox(
-                height: 40.0,
-              ),
-              LabelOfTextFormFiled(
-                label: 'USERNAME',
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              DefaultInput(
-                controller: usernameController,
-                type: TextInputType.text,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'username must be not null';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              LabelOfTextFormFiled(
-                label: 'PASSWORD',
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              DefaultInput(
-                controller: passwordController,
-                obscureText: passwordIsHidden,
-                type: TextInputType.visiblePassword,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'password must be not null';
-                  }
-                  return null;
-                },
-                suffixIcon:
-                    passwordIsHidden ? Icons.visibility : Icons.visibility_off,
-                onTapSuffixIcon: () {
-                  setState(() {
-                    passwordIsHidden = !passwordIsHidden;
-                  });
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                width: double.infinity,
-                height: 50.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    if (formKey.currentState!.validate()) {}
-                  },
-                  height: 50.0,
-                  child: Text(
-                    'Sign In',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      letterSpacing: 0.5,
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Form(
+                        key: formKey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Image.asset(
+                              'log_in_screen/icon_left.png',
+                            ),
+                            SizedBox(
+                              height: width * 0.01,
+                            ),
+                            Text(
+                              'Sign In',
+                              style: TextStyle(
+                                fontSize: 42.0,
+                              ),
+                            ),
+                            SizedBox(
+                              height: width * 0.02,
+                            ),
+                            Container(
+                              width: width * 0.25,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  LabelOfTextFormFiled(
+                                    label: 'EMAIL',
+                                  ),
+                                  SizedBox(
+                                    height: width * 0.01,
+                                  ),
+                                  DefaultInput(
+                                    controller: emailController,
+                                    type: TextInputType.text,
+                                    validator: (value) {
+                                      if (value!.isEmpty) {
+                                        return 'email must be not null';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  SizedBox(
+                                    height: width * 0.02,
+                                  ),
+                                  LabelOfTextFormFiled(
+                                    label: 'PASSWORD',
+                                  ),
+                                  SizedBox(
+                                    height: width * 0.01,
+                                  ),
+                                  DefaultInput(
+                                    controller: passwordController,
+                                    obscureText: passwordIsHidden,
+                                    type: TextInputType.visiblePassword,
+                                    validator: (value) {
+                                      if (value!.isEmpty) {
+                                        return 'password must be not null';
+                                      }
+                                      return null;
+                                    },
+                                    suffixIcon: passwordIsHidden
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                    onTapSuffixIcon: () {
+                                      setState(() {
+                                        passwordIsHidden = !passwordIsHidden;
+                                      });
+                                    },
+                                  ),
+                                  SizedBox(
+                                    height: width * 0.02,
+                                  ),
+                                  DefaultButton(
+                                    title: 'Sign In',
+                                    onPressed: (){
+                                      if (formKey.currentState!.validate()){
+
+                                      }
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                  focusColor: Colors.green,
-                  elevation: 0.0,
-                  focusElevation: 0.0,
-                  hoverElevation: 0.0,
-                  highlightElevation: 0.0,
-                  color: Colors.green,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Stack(
+                          alignment: Alignment.bottomLeft,
+                          children: [
+                            Image.asset('log_in_screen/photo_right.png'),
+                            Stack(
+                              children: [
+                                Opacity(
+                                  opacity: 0.5,
+                                  child: Container(
+                                    color: Colors.black,
+                                    height: 220,
+                                  ),
+                                ),
+                                Container(
+                                  width: double.infinity,
+                                  child: Padding(
+                                    padding: const EdgeInsetsDirectional.only(
+                                      top: 5.0,
+                                      bottom: 143.0,
+                                      start: 10.0,
+                                    ),
+                                    child: Text(
+                                      'Welcome to Dar Elkahraba .',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 36.0,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
+                ],
+              ),
+              SizedBox(height: height * 0.01,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('If you don\'t have an account'),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignUpScreen(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
               ),
             ],
           ),
